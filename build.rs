92c3f409dcd24bdee83a34ff7602e34d1749fa7b
Vladas Zakrevskis <146100@gmail.com>
2022-03-30 01:39:05 +0300

    fmt

--- build.rs
+++ build.rs
@@ -28,7 +28,6 @@ fn android_home_dir() -> String {
 }
 
 fn ndk_include_dir() -> PathBuf {
-
     if let Ok(ndk_home) = env::var("NDK_HOME") {
         return (ndk_home + "/sysroot/usr/include").into();
     }
